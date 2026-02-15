e519f0e3f166bcd705e7c10af21f4f14aac29bc4
Samir Sah <sahsamir2004.com>
2026-02-16 02:21:23 +0530

    Update dependencies and components

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Layout from "./layout.jsx";
+import { createBrowserRouter,createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}/>
+  )
+)
+  
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
